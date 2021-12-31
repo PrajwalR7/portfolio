389964b965fa8790c2197406650e61b7ab623f9c
Prajwal R <rameshprajwal21@gmail.com>
2021-12-31 18:13:09 +0530

    added intro

--- src/App.js
+++ src/App.js
@@ -14,16 +14,18 @@ import {useMediaQuery} from '@chakra-ui/media-query';
 import Project from './components/Project';
 import MeImage from './resources/me.jpg';
 import About from './components/About'
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef,useState } from 'react';
 import { useToast } from '@chakra-ui/react'
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import {motion} from 'framer-motion';
 import Typewriter from 'typewriter-effect';
+import Loader from './components/Loader';
 
 
 function App() {
   
+  const [loader,isLoaderTrue] = useState(true);
   const contactRef = useRef(null);
   const MotionText = motion(Text);
   const MotionButton = motion(Button);
@@ -47,135 +49,152 @@ function App() {
     window.scrollTo(0,givenRef.current.offsetTop)
   }
 
+  const loaderFunc = () => {
+    setTimeout(() => {
+      isLoaderTrue(false);
+    },2000)
+    return <Loader />
+  }
+
   return (
-    <motion.div className="home"
-      initial={{
-        backgroundColor:'transparent'
-      }}
-      animate={{
-        backgroundColor:'initial'
-      }}
-      transition={{
-        duration:2,
-      }}
-    >
-      <VStack 
-      backgroundColor={isDark ? "#252525" : "#16e7c7b8"}>
-        <Flex 
-        padding="5" 
-        width="100%" 
-        bgColor={isDark ? "#08fdd8" : "#1d1d1d"}
-        color={isDark ? "white" : "#10141c"}>
-          <Text 
-          fontSize={isNotSmallerScreen ? "3xl" : "lg"} 
-          ml={isNotSmallerScreen ? "10" : 0}
-          alignSelf={isNotSmallerScreen ? "flex-start" : "center"} 
-          color={isDark ? "#1d1d1d" : "#08fdd8"}>Welcome</Text>
-          
-          {/* {
-            isNotSmallerScreen ?  */}
-            <>
-              <Spacer></Spacer>
-              <IconButton 
-              backgroundColor={isDark ? "#1a202c" : "white"} 
-              color={isDark ? "white" : "#1a202c"} 
-              icon={isDark ? <FaSun /> : <FaMoon />} 
-              onClick={toggleColorMode} 
-              isRound="true"></IconButton>
-            </>
-          {/* : null
-          } */}
-        </Flex>
-        <Flex 
-        padding={isNotSmallerScreen ? "32" : "12"} 
-        spacing="200px" 
-        width="100%" 
-        direction={isNotSmallerScreen ? "column" : "column"}
-        backgroundColor={isDark ? "#252525" : "#56edd6"}>
-          <Box 
+    <div>
+      {
+        loader === true 
+          ? 
+          loaderFunc()
+          :
+      <motion.div className="home"
+        
+        initial={{
+          opacity:0
+        }}
+        animate={{
+          opacity:1
+        }}
+        transition={{
+          duration:2,
+        }}
+      >
+        
+        <VStack 
+        backgroundColor={isDark ? "#252525" : "#16e7c7b8"}>
+          <Flex 
+          padding="5" 
           width="100%" 
-          alignSelf="center" 
-          mt={isNotSmallerScreen ? 0 : 16}>
-            <MotionText 
-            initial={{
-              x:'-100vw'
-            }}
-            animate={{
-              x:0
-            }}
-            transition={{
-              delay:1,
-              duration:0.25,
-              type:"spring",
-              stiffness:120
-            }}
-             fontSize={isNotSmallerScreen ? "6xl" : "2xl"} 
-            fontWeight="semibold" 
-            alignSelf="flex-start">Hi, I'm</MotionText>
-            <MotionText
-              mt={isNotSmallerScreen ? 0 : "2"}
-              mb={isNotSmallerScreen ? 0 : "5"}
-              
-              fontSize={isNotSmallerScreen ? "6xl" : "lg"} 
-              fontWeight="bold" 
-              // bgGradient={isDark ? "linear(to-r,cyan.400,blue.400,purple.400)" : "linear(to-r,cyan.800,blue.800,purple.800)" } 
-              // bgClip="text" 
-              alignSelf="self-start">
-              <div className="name">
-                <h1 className={isDark ? 'myname_dark' : "myname"}>PRAJWAL RAMESH</h1>
-              </div>
-                </MotionText>
-            <MotionText
+          bgColor={isDark ? "#08fdd8" : "#1d1d1d"}
+          color={isDark ? "white" : "#10141c"}>
+            <Text 
+            fontSize={isNotSmallerScreen ? "3xl" : "lg"} 
+            ml={isNotSmallerScreen ? "10" : 0}
+            alignSelf={isNotSmallerScreen ? "flex-start" : "center"} 
+            color={isDark ? "#1d1d1d" : "#08fdd8"}>Welcome</Text>
+            
+            {/* {
+              isNotSmallerScreen ?  */}
+              <>
+                <Spacer></Spacer>
+                <IconButton 
+                backgroundColor={isDark ? "#1a202c" : "white"} 
+                color={isDark ? "white" : "#1a202c"} 
+                icon={isDark ? <FaSun /> : <FaMoon />} 
+                onClick={toggleColorMode} 
+                isRound="true"></IconButton>
+              </>
+            {/* : null
+            } */}
+          </Flex>
+          <Flex 
+          padding={isNotSmallerScreen ? "32" : "12"} 
+          spacing="200px" 
+          width="100%" 
+          direction={isNotSmallerScreen ? "column" : "column"}
+          backgroundColor={isDark ? "#252525" : "#56edd6"}>
+            <Box 
+            width="100%" 
+            alignSelf="center" 
+            mt={isNotSmallerScreen ? 0 : 16}>
+              <MotionText 
               initial={{
-                y:100,
-                opacity:0
+                x:'-100vw'
               }}
               animate={{
-                y:0,
-                opacity:1
+                x:0
               }}
               transition={{
-                delay:1.3,
+                delay:1,
                 duration:0.25,
                 type:"spring",
                 stiffness:120
               }}
-               
-              fontSize={isNotSmallerScreen ? "3xl" : "2xl" } 
-              mr="5" 
-              color={isDark ? "#08fdd8" : "#1d1d1d"}>
-                <Typewriter
-                  options={{
-                    strings: ['Student', 'Developer','No Michael no!!'],
-                    autoStart: true,
-                    loop: true,
-                    delay:2,
-                    deleteSpeed:1.5
-                  }}
-                />
-            </MotionText>
-          </Box>
-          <MotionButton
-          onClick={() => scrollTo(contactRef)} 
-          initial={{opacity:0}} 
-          animate={{opacity:1}} 
-          alignSelf={isNotSmallerScreen ? "flex-start" :"center"} 
-          mt="10" 
-          width={isNotSmallerScreen ? "15%" : "50%"}
-          _hover={{
-            backgroundColor:isDark ? "white" : "#1d1d1d",
-            color:isDark ? "#10141c" : "white",
-            transform:"scale(1.1)"
-          }}
-          transition="all 300ms linear"
-          >Contact me</MotionButton>
-        </Flex>
-        <Project />
-        <About />
-        <Skills />
-        <Contact refernce={contactRef}/>
-      </VStack>
-    </motion.div>
+              fontSize={isNotSmallerScreen ? "6xl" : "2xl"} 
+              fontWeight="semibold" 
+              alignSelf="flex-start">Hi, I'm</MotionText>
+              <MotionText
+                mt={isNotSmallerScreen ? 0 : "2"}
+                mb={isNotSmallerScreen ? 0 : "5"}
+                
+                fontSize={isNotSmallerScreen ? "6xl" : "lg"} 
+                fontWeight="bold" 
+                // bgGradient={isDark ? "linear(to-r,cyan.400,blue.400,purple.400)" : "linear(to-r,cyan.800,blue.800,purple.800)" } 
+                // bgClip="text" 
+                alignSelf="self-start">
+                <div className="name">
+                  <h1 className={isDark ? 'myname_dark' : "myname"}>PRAJWAL RAMESH</h1>
+                </div>
+                  </MotionText>
+              <MotionText
+                initial={{
+                  y:100,
+                  opacity:0
+                }}
+                animate={{
+                  y:0,
+                  opacity:1
+                }}
+                transition={{
+                  delay:1.3,
+                  duration:0.25,
+                  type:"spring",
+                  stiffness:120
+                }}
+                
+                fontSize={isNotSmallerScreen ? "3xl" : "2xl" } 
+                mr="5" 
+                color={isDark ? "#08fdd8" : "#1d1d1d"}>
+                  <Typewriter
+                    options={{
+                      strings: ['Student', 'Developer','No Michael no!!'],
+                      autoStart: true,
+                      loop: true,
+                      delay:2,
+                      deleteSpeed:1.5
+                    }}
+                  />
+              </MotionText>
+            </Box>
+            <MotionButton
+            onClick={() => scrollTo(contactRef)} 
+            initial={{opacity:0}} 
+            animate={{opacity:1}} 
+            alignSelf={isNotSmallerScreen ? "flex-start" :"center"} 
+            mt="10" 
+            width={isNotSmallerScreen ? "15%" : "50%"}
+            _hover={{
+              backgroundColor:isDark ? "white" : "#1d1d1d",
+              color:isDark ? "#10141c" : "white",
+              transform:"scale(1.1)"
+            }}
+            transition="all 300ms linear"
+            >Contact me</MotionButton>
+          </Flex>
+          <Project />
+          <About />
+          <Skills />
+          <Contact refernce={contactRef}/>
+        </VStack>
+      </motion.div>
+      }
+    </div>
   );
 }
 
---
+++ src/components/Loader.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { 
+  Flex,
+  IconButton, 
+  Spacer,
+  VStack,
+  Text, 
+  Box,
+  Button,
+  HStack} from '@chakra-ui/react';
+import {motion} from 'framer-motion';
+import {useMediaQuery} from '@chakra-ui/media-query';
+
+export default function Loader() {
+
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+
+  return (
+    <div>
+      <HStack 
+      backgroundColor="black"
+      w="100%"
+      h="100vh"
+      display='flex' direction="row" justify="center" alignItems="center">
+        <Text
+        className="Prajwal"
+        // initial={{x:100,opacity:0}}
+        // animate={{x:0,opacity:1}}
+        // transition={{duration:2,delay:0.5}}
+        fontWeight="hairline" p={isNotSmallerScreen ? "5" : "2"} fontFamily="'Exo', sans-serif" fontSize={isNotSmallerScreen ? "5xl" : "2xl"}>Prajwal</Text>
+        <div className='middle-carat'></div>
+        <Text
+        className='Ramesh'
+        fontWeight="extrabold" color="teal"p={isNotSmallerScreen ? "5" : "2"} fontFamily="'Exo', sans-serif" fontSize={isNotSmallerScreen ? "5xl" : "2xl"}>Ramesh</Text>
+      </HStack>
+    </div>
+  )
+}
